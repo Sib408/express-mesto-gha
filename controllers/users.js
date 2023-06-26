aa0f70ebdee1b2406037550eeb2b72184554576c
Irina Stepanets <sib408@yandex.ru>
2023-06-26 14:10:23 +0300

    update UserAvatartUpdate

--- controllers/users.js
+++ controllers/users.js
@@ -62,10 +62,8 @@ const updateUser = (req, res) => {
 
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(
-    req.user._id,
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
 
-  )
     .then((user) => {
       if (!user) {
         return res.status(404).send({ message: 'Пользователь не найден' });
