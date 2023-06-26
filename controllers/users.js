2f7b01eb6d1f96f30119c3497043e12eb3b2e70f
Irina Stepanets <sib408@yandex.ru>
2023-06-26 14:05:34 +0300

    update UserUpdate

--- controllers/users.js
+++ controllers/users.js
@@ -45,9 +45,7 @@ const createUsers = (req, res) => {
 
 const updateUser = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(
-    req.user._id,
-  )
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
         return res.status(404).send({ message: 'Пользователь не найден' });
