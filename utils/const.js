056f1d6b1efa51be6a3828bce98a2eb6e6bff1e4
Irina Stepanets <sib408@yandex.ru>
2023-06-28 19:22:56 +0300

    update after review

--- utils/const.js
+++ utils/const.js
@@ -1,9 +1,8 @@
 const http2 = require('node:http2');
 
-const STATUS_OK = 200; // 200;
-const STATUS_CREATED = 201; // 201;
+const STATUS_OK = http2.constants.HTTP_STATUS_STATUS_OK; // 200;
+const STATUS_CREATED = http2.constants.HTTP_STATUS_STATUS_CREATED; // 201;
 const BAD_REQUEST_ERROR = http2.constants.HTTP_STATUS_BAD_REQUEST; // 400
-//const BAD_REQUEST_ERROR = http2.constants.HTTP_STATUS_BAD_REQUEST; // 400
 const NOT_FOUND_ERROR = http2.constants.HTTP_STATUS_NOT_FOUND; // 404
 const ERROR_SERVER = http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR; // 500
 
