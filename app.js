5d1450925fbfe8d36238c1dfeecb93fedb74b66d
Irina Stepanets <sib408@yandex.ru>
2023-07-01 15:18:03 +0300

    14 sprint

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes');
+const { errors } = require('celebrate');
 
 const { PORT = 3000 } = process.env;
 
@@ -9,6 +10,14 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+app.use(errors());
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64994b3fbed133c146f3c834'
+  };
+
+  next();
+});
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
